c8a1203f249fb079de8f5d798b1f38b9f20d276e
Michal Choma <michalkk04@gmail.com>
2026-07-20 13:52:01 +0200

    added dependencies

---
+++ backend/target/debug/deps/backend-76ab8390f1beb115.d
@@ -0,0 +1,5 @@
+/home/mchoma/transcendece/backend/target/debug/deps/backend-76ab8390f1beb115.d: src/main.rs
+
+/home/mchoma/transcendece/backend/target/debug/deps/libbackend-76ab8390f1beb115.rmeta: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/backend-bee87ce098901144.d
@@ -0,0 +1,5 @@
+/home/mchoma/transcendece/backend/target/debug/deps/backend-bee87ce098901144.d: src/main.rs
+
+/home/mchoma/transcendece/backend/target/debug/deps/libbackend-bee87ce098901144.rmeta: src/main.rs
+
+src/main.rs:
