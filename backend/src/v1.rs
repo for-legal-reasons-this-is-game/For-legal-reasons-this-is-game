e760d7de29c5ae677660e5c48742e66a4cb7b33d
Michal Choma <michalkk04@gmail.com>
2026-07-21 13:57:31 +0200

    feature: split main to files

---
+++ backend/src/v1.rs
@@ -0,0 +1,71 @@
+use axum::{
+    extract::{Json, Path},
+    http::{Response, StatusCode},
+};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize)]
+pub struct CreateUser {
+    name: String,
+}
+
+//create user write it to database and respond if it sucseeds and create userid
+pub async fn create_user(Json(payload): Json<Value>) -> Result<Response<String>, StatusCode> {
+    let data: CreateUser = serde_json::from_value(payload).unwrap();
+    println!("{}", data.name);
+    todo!();
+}
+
+//create account with unique ID and write it to database
+pub async fn create_account(Json(payload): Json<Value>) -> Result<Response<String>, StatusCode> {
+    todo!();
+}
+
+//returns with information about the user
+pub async fn fetch_user(Path(user_id): Path<Uuid>) -> Result<Response<String>, StatusCode> {
+    todo!();
+}
+
+//returns with information about the account
+pub async fn fetch_account(Path(account_id): Path<Uuid>) -> Result<Response<String>, StatusCode> {
+    todo!();
+}
+
+//returns with information about the accoiunts position
+pub async fn fetch_positions(Path(account_id): Path<Uuid>) -> Result<Response<String>, StatusCode> {
+    todo!();
+}
+
+//returns with information about the accoiunts position
+pub async fn fetch_account_position_asset(
+    Path(account_id): Path<Uuid>,
+    Path(asset_id): Path<Uuid>,
+) -> Result<Response<String>, StatusCode> {
+    todo!();
+}
+
+//returns with information about the accoiunts position
+pub async fn delete_account(
+    Path(account_id): Path<Uuid>,
+    Path(asset_id): Path<Uuid>,
+) -> Result<Response<String>, StatusCode> {
+    todo!();
+}
+
+pub async fn list_instruments() -> Result<Response<String>, StatusCode> {
+    todo!();
+}
+
+pub async fn fetch_instrument(Path(symbol): Path<String>) -> Result<Response<String>, StatusCode> {
+    todo!();
+}
+
+pub async fn create_instrument(Path(symbol): Path<String>) -> Result<Response<String>, StatusCode> {
+    todo!();
+}
+
+pub async fn delete_instrument(Path(symbol): Path<String>) -> Result<Response<String>, StatusCode> {
+    todo!();
+}
