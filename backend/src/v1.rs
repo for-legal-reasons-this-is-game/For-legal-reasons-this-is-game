fa25a77ada1ca543d2447de760fe39951c15e703
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-08-09 19:35:30 +0200

    feat: connection to db added, migration in process to real back end

--- backend/src/v1.rs
+++ backend/src/v1.rs
@@ -1,25 +1,44 @@
 use axum::{
-    extract::{Json, Path},
+    extract::{Json, Path, State},
     http::{Response, StatusCode},
 };
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use sqlx::{FromRow, PgPool};
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize)]
-pub struct CreateUser {
+#[derive(Deserialize)]
+pub struct UserPayload {
     name: String,
 }
 
+#[derive(Serialize, FromRow)]
+pub struct User {
+    id: i32,
+    name: String,
+}
+#[derive(Clone)]
+pub struct AppState {
+    pub pg_connections: PgPool,
+}
 //create user write it to database and respond if it sucseeds and create userid
-pub async fn create_user(Json(payload): Json<Value>) -> Result<Response<String>, StatusCode> {
-    let data: CreateUser = serde_json::from_value(payload).unwrap();
-    println!("{}", data.name);
-    todo!();
+pub async fn create_user(
+    State(state): State<AppState>,
+    Json(payload): Json<UserPayload>,
+) -> Result<(StatusCode, Json<User>), StatusCode> {
+    sqlx::query_as::<_, User>("INSERT INTO users name VALUES $1 RETURNING * ")
+        .bind(payload.name)
+        .fetch_one(&state.pg_connections)
+        .await
+        .map(|u| (StatusCode::CREATED, Json(u)))
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
 
 //create account with unique ID and write it to database
-pub async fn create_account(Json(_payload): Json<Value>) -> Result<Response<String>, StatusCode> {
+pub async fn create_account(
+    State(_state): State<AppState>,
+    Json(_payload): Json<Value>,
+) -> Result<Response<String>, StatusCode> {
     todo!();
 }
 
