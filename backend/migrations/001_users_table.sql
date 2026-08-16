bf4664d62188c59269bc31d7e1443f1eee3cbe85
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-08-16 21:08:32 +0200

    feat: continue migration. Made user ids UUIDs for security

--- backend/migrations/001_users_table.sql
+++ backend/migrations/001_users_table.sql
@@ -1,5 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL UNIQUE
+  user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_name TEXT NOT NULL UNIQUE
 );
+-- CREATE TABLE IF NOT EXISTS accounts (
+--   account_id SERIAL PRIMARY KEY,
+--   account_name TEXT NOT NULL UNIQUE
+--   FOREIGN KEY (user_id) REFERENCES
+--   users(user_id);
+-- );
 
