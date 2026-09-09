1ab0648ac4e8417fed3a633e85c1dbe40a9d7ced
YousefElghanam <yousef.elghanam@gmail.com>
2026-09-09 16:25:40 +0200

    lear: react list keys and passing values to a component

--- frontend/src/src/App.tsx
+++ frontend/src/src/App.tsx
@@ -1,125 +1,46 @@
-// function App() {
-//     return (
-//         <>
-//             <h1>HELLO WORLD!</h1>
-//         </>
-//     );
-// }
+type Market = {
+    coin: string;
+    price: number;
+    change: number;
+};
 
-// export default App;
-
-import { useState } from "react";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import "./App.css";
+function Header({ coin, price, change }: Market) {
+    return (
+        <header>
+            <p>{coin}</p>
+            <p>{price}</p>
+            <p>{change.toFixed(2)}%</p>
+        </header>
+    );
+}
 
 function App() {
-    const [count, setCount] = useState(0);
-
+    const markets: Market[] = [
+        {
+            coin: "BTC/USD",
+            price: 67000,
+            change: 2.5,
+        },
+        {
+            coin: "ETH/USD",
+            price: 4500,
+            change: -1.2,
+        },
+        {
+            coin: "SOL/USD",
+            price: 180,
+            change: 4.7,
+        },
+    ];
     return (
         <>
-            <section id="center">
-                <div className="hero">
-                    <img src={heroImg} className="base" width="170" height="179" alt="" />
-                    <img src={reactLogo} className="framework" alt="React logo" />
-                    <img src={viteLogo} className="vite" alt="Vite logo" />
+            <h1>My Trading Platform</h1>
+            {markets.map((market) => (
+                <div key={market.coin}>
+                    <Header {...market} />
+                    <br />
                 </div>
-                <div>
-                    <h1>Get started</h1>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-                    </p>
-                </div>
-                <button
-                    type="button"
-                    className="counter"
-                    onClick={() => setCount((count) => count + 1)}>
-                    Count is {count}
-                </button>
-            </section>
-
-            <div className="ticks"></div>
-
-            <section id="next-steps">
-                <div id="docs">
-                    <svg className="icon" role="presentation" aria-hidden="true">
-                        <use href="/icons.svg#documentation-icon"></use>
-                    </svg>
-                    <h2>Documentation</h2>
-                    <p>Your questions, answered</p>
-                    <ul>
-                        <li>
-                            <a href="https://vite.dev/" target="_blank">
-                                <img className="logo" src={viteLogo} alt="" />
-                                Explore Vite
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://react.dev/" target="_blank">
-                                <img className="button-icon" src={reactLogo} alt="" />
-                                Learn more
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div id="social">
-                    <svg className="icon" role="presentation" aria-hidden="true">
-                        <use href="/icons.svg#social-icon"></use>
-                    </svg>
-                    <h2>Connect with us</h2>
-                    <p>Join the Vite community</p>
-                    <ul>
-                        <li>
-                            <a href="https://github.com/vitejs/vite" target="_blank">
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true">
-                                    <use href="/icons.svg#github-icon"></use>
-                                </svg>
-                                GitHub
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://chat.vite.dev/" target="_blank">
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true">
-                                    <use href="/icons.svg#discord-icon"></use>
-                                </svg>
-                                Discord
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://x.com/vite_js" target="_blank">
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true">
-                                    <use href="/icons.svg#x-icon"></use>
-                                </svg>
-                                X.com
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true">
-                                    <use href="/icons.svg#bluesky-icon"></use>
-                                </svg>
-                                Bluesky
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </section>
-
-            <div className="ticks"></div>
-            <section id="spacer"></section>
+            ))}
         </>
     );
 }
